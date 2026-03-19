06cff72f71c2416168e00c7549bfc75daf8504bd
Oto Soares Coelho Junior <oto.coelho-junior@serpro.gov.br>
2026-03-19 13:53:36 -0300

    Configuração de Testcontainers

---
+++ src/test/java/br/gov/serpro/otojunior/exemplo/dominio/produto/ProdutoTest.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package br.gov.serpro.otojunior.exemplo.dominio.produto;
+
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Classe de teste para a classe Produto.
+ * @author Oto Soares Coelho Junior (oto.coelho-junior@serpro.gov.br)
+ * @since 18/03/2026 - SERPRO 2026
+ */
+@Disabled
+@DisplayName("Produto")
+class ProdutoTest {
+    /**
+     * Teste de métodos get de produto com sucesso.
+     */
+    @Test
+    @DisplayName("Métodos get de produto com sucesso")
+    void gets_sucesso() {
+        fail("Not yet implemented");
+    }
+    
+    /**
+     * Teste de métodos set de produto com sucesso.
+     */
+    @Test
+    @DisplayName("Métodos set de produto com sucesso")
+    void sets_sucesso() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Teste de hashCode de produto com sucesso.
+     */
+    @Test
+    @DisplayName("Hashcode de produto com sucesso")
+    void hashcode_sucesso() {
+        fail("Not yet implemented");
+    }
+    
+    /**
+     * Teste de equals de produto com sucesso.
+     */
+    @Test
+    @DisplayName("Equals de produto com sucesso")
+    void equals_sucesso() {
+        fail("Not yet implemented");
+    }
+}
